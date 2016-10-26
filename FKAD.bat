b97cf66a776ee869ae85da9d1fc5f918e8ba76d3
haldak <haldakcis@gmail.com>
2016-10-26 16:25:08 +0800

    Delete these files then AD15 can start again.

---
+++ FKAD.bat
@@ -0,0 +1,7 @@
+del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\ADVPCB.DFT"
+del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\Advsch.dft"
+del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\Advsch.MMsdft"
+del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\DXP.RAF"
+del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\DXP.RCS"
+del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\UserTools.TLT"
+echo. & pause
