6382e220be6c03947fdaf0cd2071bf64a990ed45
haldak <haldakcis@gmail.com>
2016-11-06 16:49:38 +0800

    It works!

---
+++ ADhelper.bat
@@ -0,0 +1,58 @@
+@echo off
+echo Stop DXP.exe ...
+rem tskill DXP
+
+echo Delete ADVPCB.DFT
+dir /s/a/b C:\Users\ADVPCB.DFT >tempFile
+set /p tempVal=<tempFile
+rem echo %tempVal%
+for /f "delims=" %%f in ('dir /b /s /a-d tempFile') do (
+	if %%~zf. neq 0. del "%tempVal%"
+)
+
+
+echo Delete Advsch.dft
+dir /s/a/b C:\Users\Advsch.dft >tempFile
+set /p tempVal=<tempFile
+rem echo %tempVal%
+for /f "delims=" %%f in ('dir /b /s /a-d tempFile') do (
+	if %%~zf. neq 0. del "%tempVal%"
+)
+
+echo Delete Advsch.MMsdft
+dir /s/a/b C:\Users\Advsch.MMsdft >tempFile
+set /p tempVal=<tempFile
+rem echo %tempVal%
+for /f "delims=" %%f in ('dir /b /s /a-d tempFile') do (
+	if %%~zf. neq 0. del "%tempVal%"
+)
+
+echo Delete DXP.RAF
+dir /s/a/b C:\Users\DXP.RAF >tempFile
+set /p tempVal=<tempFile
+rem echo %tempVal%
+for /f "delims=" %%f in ('dir /b /s /a-d tempFile') do (
+	if %%~zf. neq 0. del "%tempVal%"
+)
+
+echo Delete DXP.RCS
+dir /s/a/b C:\Users\DXP.RCS >tempFile
+set /p tempVal=<tempFile
+rem echo %tempVal%
+for /f "delims=" %%f in ('dir /b /s /a-d tempFile') do (
+	if %%~zf. neq 0. del "%tempVal%"
+)
+
+echo Delete UserTools.TLT
+dir /s/a/b C:\Users\UserTools.TLT >tempFile
+set /p tempVal=<tempFile
+rem echo %tempVal%
+for /f "delims=" %%f in ('dir /b /s /a-d tempFile') do (
+	if %%~zf. neq 0. del "%tempVal%"
+)
+
+del tempFile
+
+echo Done!Enjoy:)
+
+pause
--- FKAD.bat
+++
@@ -1,7 +0,0 @@
-del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\ADVPCB.DFT"
-del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\Advsch.dft"
-del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\Advsch.MMsdft"
-del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\DXP.RAF"
-del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\DXP.RCS"
-del "C:\Users\haldak\AppData\Roaming\Altium\Altium Designer {FDBAE78B-D20D-465A-B7ED-ABDF8C512CE2}\UserTools.TLT"
-echo. & pause
