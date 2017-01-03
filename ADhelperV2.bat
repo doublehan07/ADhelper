c70202bc47800e237a274d76819f5e7c98244069
haldak <haldakcis@gmail.com>
2017-01-04 04:02:50 +0800

    For personal use.

--- ADhelperV2.bat
+++ ADhelperV2.bat
@@ -6,48 +6,36 @@ echo Delete ADVPCB.DFT
 for /f "delims=" %%f in ('dir /s/a/b C:\Users\ADVPCB.DFT') do (
 	del "%%f"
 )
-for /f "delims=" %%f in ('dir /s/a/b C:\Documents and Settings\ADVPCB.DFT') do (
-	del "%%f"
 )
 
 echo Delete Advsch.dft
 for /f "delims=" %%f in ('dir /s/a/b C:\Users\Advsch.dft') do (
 	del "%%f"
 )
-for /f "delims=" %%f in ('dir /s/a/b C:\Documents and Settings\Advsch.dft') do (
-	del "%%f"
 )
 
 echo Delete Advsch.MMsdft
 for /f "delims=" %%f in ('dir /s/a/b C:\Users\Advsch.MMsdft') do (
 	del "%%f"
 )
-for /f "delims=" %%f in ('dir /s/a/b C:\Documents and Settings\Advsch.MMsdft') do (
-	del "%%f"
 )
 
 echo Delete DXP.RAF
 for /f "delims=" %%f in ('dir /s/a/b C:\Users\DXP.RAF') do (
 	del "%%f"
 )
-for /f "delims=" %%f in ('dir /s/a/b C:\Documents and Settings\DXP.RAF') do (
-	del "%%f"
 )
 
 echo Delete DXP.RCS
 for /f "delims=" %%f in ('dir /s/a/b C:\Users\DXP.RCS') do (
 	del "%%f"
 )
-for /f "delims=" %%f in ('dir /s/a/b C:\Documents and Settings\DXP.RCS') do (
-	del "%%f"
 )
 
 echo Delete UserTools.TLT
 for /f "delims=" %%f in ('dir /s/a/b C:\Users\UserTools.TLT') do (
 	del "%%f"
 )
-for /f "delims=" %%f in ('dir /s/a/b C:\Documents and Settings\UserTools.TLT') do (
-	del "%%f"
 )
 
 echo Done!Enjoy:)
